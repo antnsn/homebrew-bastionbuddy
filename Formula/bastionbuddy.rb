9ee49d4787a938fb179aa19dd165eb7c923b7b69
GitHub Action <action@github.com>
2025-01-13 12:27:06 +0000

    Update formula to version v1.0.0

--- Formula/bastionbuddy.rb
+++ Formula/bastionbuddy.rb
@@ -1,25 +1,25 @@
 class Bastionbuddy < Formula
   desc "CLI tool for managing Azure Bastion connections"
   homepage "https://github.com/antnsn/BastionBuddy"
-  version "0.1.0"
+  version "v1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/antnsn/BastionBuddy/releases/download/v#{version}/bastionbuddy_#{version}_darwin_arm64.tar.gz"
-      sha256 "" # You'll need to add the SHA256 of the release file
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/antnsn/BastionBuddy/releases/download/v#{version}/bastionbuddy_#{version}_darwin_amd64.tar.gz"
-      sha256 "" # You'll need to add the SHA256 of the release file
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/antnsn/BastionBuddy/releases/download/v#{version}/bastionbuddy_#{version}_linux_arm64.tar.gz"
-      sha256 "" # You'll need to add the SHA256 of the release file
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/antnsn/BastionBuddy/releases/download/v#{version}/bastionbuddy_#{version}_linux_amd64.tar.gz"
-      sha256 "" # You'll need to add the SHA256 of the release file
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
