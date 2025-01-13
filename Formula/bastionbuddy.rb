b559c5fa75a26269c5c9b47e7f0ca1e274a079e3
GitHub Action <action@github.com>
2025-01-13 13:09:10 +0000

    Update formula to version v1.0.0

--- Formula/bastionbuddy.rb
+++ Formula/bastionbuddy.rb
@@ -6,20 +6,20 @@ class Bastionbuddy < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.0/bastionbuddy_darwin_arm64.zip"
-      sha256 "8cd3d8a2b77d72207bf8a2745e5368a7790ad9e6c4ac9f0c06a73993c0391fe5"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.0/bastionbuddy_darwin_amd64.zip"
-      sha256 "1d6851ece77c48bfd0f2c8747b1e4ebf8c9b312c67b3d8a1548dec441c248529"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.0/bastionbuddy_linux_arm64.zip"
-      sha256 "be752755502634e495527fd1eac207ae11280a37fa2c7afe7218fc98169e4391"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.0/bastionbuddy_linux_amd64.zip"
-      sha256 "3954dbb1a02585194d64209f058c0f903e8f113fb9101b2dc2186d495c06f62f"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
