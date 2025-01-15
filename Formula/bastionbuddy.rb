3878014281b29dfb3e237f9c0b5a765fe8db5744
GitHub Action <action@github.com>
2025-01-15 08:54:23 +0000

    Update formula to version 1.0.3

--- Formula/bastionbuddy.rb
+++ Formula/bastionbuddy.rb
@@ -1,25 +1,25 @@
 class Bastionbuddy < Formula
   desc "CLI tool for managing Azure Bastion connections"
   homepage "https://github.com/antnsn/BastionBuddy"
-  version "1.0.2"
+  version "1.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.2/bastionbuddy_darwin_arm64.tar.gz"
-      sha256 "2bc14fa9c31366490c2a15ae3ad1a250beb37342c94305b099019295f25b5b03"
+      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.3/bastionbuddy_darwin_arm64.tar.gz"
+      sha256 "2d6b19bcbfa92429b9ba745441fe81ce022a595aed6d495378cf28df35dba8c6"
     else
-      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.2/bastionbuddy_darwin_amd64.tar.gz"
-      sha256 "5fd618dcc7572f36ae7060773c1521fc6c79f82c92d46002130ed6929a5ec30e"
+      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.3/bastionbuddy_darwin_amd64.tar.gz"
+      sha256 "e6b8a2cb4fddac46474481853147ace8df94305c4b066346782ad0b6d2093dc4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.2/bastionbuddy_linux_arm64.tar.gz"
-      sha256 "4f80faa7ff36c6abb428e4e5391353564237a3502407a4a58c28efe4485066d4"
+      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.3/bastionbuddy_linux_arm64.tar.gz"
+      sha256 "a8c08982ef012fdbc6cc15b7f64fde5146a42fdba46d84239568c3e05f6e8a60"
     else
-      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.2/bastionbuddy_linux_amd64.tar.gz"
-      sha256 "649986448f12a6000bd0561e696dbed93e44ed7d1bbcc4788044fa2408322b42"
+      url "https://github.com/antnsn/BastionBuddy/releases/download/v1.0.3/bastionbuddy_linux_amd64.tar.gz"
+      sha256 "a8b2de7a2785b03954b6fee54395fbca26fb58d6b51b0a2f8bb84760f17b4e6e"
     end
   end
 
